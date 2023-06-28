d1e150b7bd0c8548adb8c12c9194866c2dbc4c09
kakuho <m1m23a07@oit.ac.jp>
2023-06-28 15:15:35 +0900

    kadai

--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -4,4 +4,47 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public double ave(int x, int y) {
+    double z = (double) (x + y) / 2;
+    return z;
+  }
+
+  public int sumall(int x, int y) {
+    int z = 0;
+    for (int i = x; i <= y; i++) {
+      z += i;
+    }
+    return z;
+  }
+
+  public double aveall(int x, int y) {
+    double z = 0;
+    int count = 0;
+    for (int i = x; i <= y; i++) {
+      z += i;
+      count += 1;
+    }
+    return z / count;
+  }
+
+  public int odd(int x, int y) {
+    int z = 0;
+    for (int i = x; i <= 10; i++) {
+      if (i % 2 == 1) {
+        z += i;
+      }
+    }
+    return z;
+  }
+
+  public int even(int x, int y) {
+    int z = 0;
+    for (int i = x; i <= 10; i++) {
+      if (i % 2 == 0) {
+        z += i;
+      }
+    }
+    return z;
+  }
 }
--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -5,9 +5,44 @@ import static org.junit.Assert.*;
 
 public class CalculateTest {
   @Test
-  public void testSum() {
+  public void testsum() {
     Calculate calcurate = new Calculate();
     int expected = 5;
     assertEquals(expected, calcurate.sum(2, 3));
+  }
+
+  @Test
+  public void testave() {
+    Calculate calcurate = new Calculate();
+    double expected = 2.5;
+    assertEquals(expected, calcurate.ave(2, 3), 0.0001);
+  }
+
+  @Test
+  public void testsumall() {
+    Calculate calcurate = new Calculate();
+    int expected = 55;
+    assertEquals(expected, calcurate.sumall(1, 10));
+  }
+
+  @Test
+  public void testaveall() {
+    Calculate calcurate = new Calculate();
+    double expected = 5.5;
+    assertEquals(expected, calcurate.aveall(1, 10), 0.0001);
+  }
+
+  @Test
+  public void testodd() {
+    Calculate calcurate = new Calculate();
+    int expected = 25;
+    assertEquals(expected, calcurate.odd(1, 10));
+  }
+
+  @Test
+  public void testeven() {
+    Calculate calcurate = new Calculate();
+    int expected = 30;
+    assertEquals(expected, calcurate.even(1, 10));
   }
 }
